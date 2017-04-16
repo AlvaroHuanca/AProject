ad0aa0a22bd21c2aa667b744e88a6f4bfadf1759
AlvaroHuanca <alvaroupeu@gmail.com>
2017-04-16 17:37:20 -0500

    Crea la plantilla de la pagina

---
+++ app/shounen/shounenCtrl.js
@@ -0,0 +1,9 @@
+(function(){
+	"use strict";
+	angular.module("shounenApp")
+		.controller('ShounenCtrl', ShounenCtrl );
+
+	function ShounenCtrl($scope) {
+		$scope.mensaje = "hola Visitante";
+	}
+}());
