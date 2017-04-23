4a00fab619aa9a46d84fad3c075ecde1f32436a7
AlvaroHuanca <alvaroupeu@gmail.com>
2017-04-23 11:24:36 -0500

    second

--- app/shounen/shounenCtrl.js
+++ app/shounen/shounenCtrl.js
@@ -1,9 +1,10 @@
 (function(){
 	"use strict";
 	angular.module("shounenApp")
-		.controller('ShounenCtrl', ShounenCtrl );
+		.controller("ShounenCtrl", ShounenCtrl );
 
 	function ShounenCtrl($scope) {
+
 		$scope.mensaje = "hola Visitante";
 	}
 }());
